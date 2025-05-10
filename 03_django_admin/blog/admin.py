c9ef331bcc3f4490b6a944f4af74d811d09b205b
Muhammad Hashim <hashiimtahir@gmail.com>
2025-05-10 15:40:25 +0500

    📝 Adding custom fields

--- 03_django_admin/blog/admin.py
+++ 03_django_admin/blog/admin.py
@@ -6,8 +6,11 @@ class BlogAdmin(admin.ModelAdmin):
     search_fields = ['title']
     show_full_result_count = True
     list_filter = ['title']
-    list_display = ['title', 'created_at']
+    list_display = ['title', 'created_at', 'letter_count']
     date_hierarchy = 'created_at'
+
+    def letter_count(self, obj):
+        return sum(1 for c in obj.content if c.isalpha())
 
 
 admin.site.register(models.Blog, BlogAdmin)
